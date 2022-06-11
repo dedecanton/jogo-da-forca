708df2abe41fe1ee59e59fdd26e95f3bb3fb4d6d
dedecanton <andrecanton18@gmail.com>
2022-06-11 14:50:06 -0300

    feat: funcao de informacoes

---
+++ informacoes.py
@@ -0,0 +1,12 @@
+def pedir_informacoes():
+    palavra_chave = input('Digite a palavra chave: ')
+    dica1 = input('Digite a dica 1: ')
+    dica2 = input('Digite a dica 2: ')
+    dica3 = input('Digite a dica 3: ')
+
+    return {
+        palavra_chave,
+        dica1,
+        dica2,
+        dica3
+    }
